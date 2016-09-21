c40727e9b674e173bd7f6ab089b8a9e7e394bd14
bgrist18 <bgrist309@gmail.com>
2016-09-21 17:11:25 -0400

    assignment2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
  * Created by brettgrist on 9/21/16.
  */
 public class Main {
+
+    static GolfClubs createItem(Scanner scanner, HashMap glf) {
+        GolfClubs add = new GolfClubs();
+
+        System.out.print("Enter Golf Club Name: ");
+        String x = scanner.nextLine();
+        if (x.equalsIgnoreCase("Calloway")){
+            add.setBrand(x);
+        }
+
+        System.out.print("Enter the item price: ");
+        String y = scanner.nextLine();
+        System.out.print("Enter the quantity: ");
+        int z = Integer.parseInt(scanner.nextLine());
+
+        glf.put(x, add);
+
+        return add;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -26,39 +46,42 @@ public class Main {
                 String option = scanner.nextLine();
                 switch (option) {
                     case "1":
-                        System.out.print("Enter Golf Club Name: ");
-                        String x = scanner.nextLine();
-                        System.out.print("Enter the item price: ");
-                        String y = scanner.nextLine();
-                        System.out.print("Enter the quantity: ");
-                        int z = Integer.parseInt(scanner.nextLine());
+                        GolfClubs add = createItem(scanner, glf);
+                        glf.put(add.brand, add);
+//                        System.out.print("Enter Golf Club Name: ");
+//                        String x = scanner.nextLine();
+//                        System.out.print("Enter the item price: ");
+//                        String y = scanner.nextLine();
+//                        System.out.print("Enter the quantity: ");
+//                        int z = Integer.parseInt(scanner.nextLine());
 
-                        if (glf.containsKey(x)) {
-                            glf.get(x).setQuantity(glf.get(x).quantity + z);
-                        } else {
-                            GolfClubs add = new GolfClubs(y, z, x);
-                            glf.put(x, add);
-                        }
+//                        if (glf.containsKey(x)) {
+//                            glf.get(x).setQuantity(glf.get(x).quantity + z);
+//                        } else {
+//                            GolfClubs add = new GolfClubs(y, z, x);
+//                            glf.put(x, add);
+//                        }
                         break;
                     case "2":
                         System.out.print("Enter Item No.: ");
-                        x = scanner.nextLine();
+                        String x = scanner.nextLine();
                         glf.remove(x);
                         break;
                     case "3":
                         for (String out : glf.keySet()) {
                             int qty = glf.get(out).quantity;
                             String priceOut = glf.get(out).price;
-                            System.out.println(out + " " + priceOut + " " + qty);
+                          //  System.out.println(out + " " + priceOut + " " + qty);
+                            System.out.printf("%s %s %s\n", out, priceOut, qty);
                         }
                         break;
                     case "4":
                         System.out.println("Enter the item number you want to update:");
-                        x = scanner.nextLine();
+                        String x2 = scanner.nextLine();
                         System.out.println("How many itmes are you adding to the current total?");
-                        z = Integer.parseInt(scanner.nextLine());
-                        int newQty = glf.get(x).quantity + z;
-                        glf.get(x).quantity = newQty;
+                        int z = Integer.parseInt(scanner.nextLine());
+                        int newQty = glf.get(x2).quantity + z;
+                        glf.get(x2).quantity = newQty;
                         break;
                     case "5":
                         isLoggedIn = false;
